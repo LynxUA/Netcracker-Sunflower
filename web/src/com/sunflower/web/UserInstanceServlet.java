f7c24382b0ff3aa7829833c954f1597f5a28cb4a
Den-K <denis.kobernik@gmail.com>
2014-11-29 19:44:34 +0200

    User's Instance Servlet

---
+++ web/src/com/sunflower/web/UserInstanceServlet.java
@@ -0,0 +1,22 @@
+package com.sunflower.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Den on 29.11.2014.
+ */
+@WebServlet(name = "UserInstanceServlet")
+public class UserInstanceServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("user_si.jsp").forward(request,response);
+    }
+}
