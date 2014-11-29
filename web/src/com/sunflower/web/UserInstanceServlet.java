9fc2004568b278390032b7f097ccae9fa567346f
Den-K <denis.kobernik@gmail.com>
2014-11-29 19:47:21 +0200

    User's Instance Servlet

--- web/src/com/sunflower/web/UserInstanceServlet.java
+++ web/src/com/sunflower/web/UserInstanceServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet(name = "UserInstanceServlet")
 public class UserInstanceServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
