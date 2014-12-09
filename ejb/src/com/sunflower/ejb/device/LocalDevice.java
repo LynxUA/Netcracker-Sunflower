b1b2e7e0c6027ff9a6723654e5bab10c2130407f
Nazar <nazarmsx@ukr.net>
2014-12-09 23:20:55 +0200

    Device EJB added.

---
+++ ejb/src/com/sunflower/ejb/device/LocalDevice.java
@@ -0,0 +1,10 @@
+package com.sunflower.ejb.device;
+
+import javax.ejb.EJBLocalObject;
+
+/**
+ * Created by denysburlakov on 08.12.14.
+ */
+public interface LocalDevice extends EJBLocalObject {
+
+}
