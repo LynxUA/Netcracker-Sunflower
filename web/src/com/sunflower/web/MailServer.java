4288d24de66f3158f73149d27b9228e256a2c3fd
Alexey Altukhov <altuhov9469@gmail.com>
2014-12-24 22:32:32 +0200

    Update MailServer.java

--- web/src/com/sunflower/web/MailServer.java
+++ web/src/com/sunflower/web/MailServer.java
@@ -10,44 +10,44 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 import javax.mail.Authenticator;
-import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
+
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 public class MailServer {
 
     private static Logger logger = Logger.getLogger(MailServer.class);
 
     protected static void messageAfterRegistration(String name, String password, String email, String login){
-        Map<String, String> rootMap = new HashMap<String, String>();//here are fields, which we wanted to paste into template
-        rootMap.put("name", name);
-        rootMap.put("login", login);
-        rootMap.put("password", password);
-        sendMessage(email, rootMap, "/Developer/Java_EE/glassfish4/glassfish/config/mail-registration-template.ftl","Registration on SunFlower");
+
+        String msg="<html> <body> Hi "+name+", <p style='color:green;'> " +
+                "Regards for registrating,<br/> " +
+                "Your user login:"+login+"<br/> " +
+                "password:"+password+"<br/>" +
+                "Respectfully,<br/> " +
+                "Accounts Team SunFlower.</p>" +
+                "</body> " +
+                "</html>";
+        sendMessage(email, msg,"Registration on SunFlower");
     }
 
-    protected static void messagePasswordReminder(String email){
+  /*  protected static void messagePasswordReminder(String email){
         Map<String, String> rootMap = new HashMap<String, String>();//here are fields, which we wanted to paste into template
         rootMap.put("name", "name from database");
         rootMap.put("login", email);
         rootMap.put("password", "password from db");
        sendMessage(email, rootMap, "mail-reminder-password-template.ftl","SunFlower remind your password");
-    }
+    }*/
 
 //general mail configuration for all templates
-    private static void sendMessage(String email, Map<String, String> rootMap, String templateName, String messageTitle){
+    private static void sendMessage(String email, String msg, String messageTitle){
         String host = "smtp.gmail.com";
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -67,7 +67,7 @@ public class MailServer {
             message.setFrom(new InternetAddress(from));
             message.setRecipients(Message.RecipientType.TO,address);
             message.setSubject(messageTitle);
-            message.setContent(makeTemplate(rootMap, readFile(templateName)), "text/html");
+            message.setContent(msg, "text/html");
             Transport.send(message);
         } catch (MessagingException e) {
             logger.error(e.getMessage(), e);
@@ -90,25 +90,5 @@ public class MailServer {
         return res;
     }
 
-    //putting fields into template file
-    private static String makeTemplate(Map<String, String> map, String s)
-    {
-        String field ="";
-        String res = "";
-        for(int i =0; i<s.length(); ++i){
-            if(s.charAt(i)=='$'){
-                i+=2;
-                while(s.charAt(i)!='}')
-                {
-                    field+=s.charAt(i);
-                    ++i;
-                }
-                ++i;
-                res+=map.get(field);
-                field="";
-            }
-            res+=s.charAt(i);
-        }
-        return res;
-    }
+
 }
